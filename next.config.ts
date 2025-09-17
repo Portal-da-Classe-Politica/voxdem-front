c788cb3b8ccc17039a46ef5228dacf38591f4f4b
Luiz D Avila <luiz@anota.ai>
2025-09-17 03:10:06 -0300

    feat: adicionar suporte a subpath no deploy do frontend VoxDem

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  basePath: '/voxdem',
+  assetPrefix: '/voxdem',
+  trailingSlash: true,
 };
 
 export default nextConfig;
