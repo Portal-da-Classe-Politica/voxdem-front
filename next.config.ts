5faf8494a7c4b0c1730e7f0d854350cdfdbefa0b
Luiz D Avila <luiz@anota.ai>
2025-09-17 18:13:50 -0300

    feat: remove standalone output and basePath configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'standalone',
-  basePath: '/voxdem',
-  assetPrefix: '/voxdem',
-  trailingSlash: true,
+ 
 };
 
 export default nextConfig;
